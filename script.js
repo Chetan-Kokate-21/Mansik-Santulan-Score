5a59abae027b8d57044d08c6018bba77bc3ab812
Chetan-Kokate <chetankokate2004@gmail.com>
2026-08-11 13:44:08 +0530

    Updated js file with deployed backend link

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const API_BASE = "https://mansik-santulan-score-yf1v.onrender.com";
+  const API_BASE = "https://mansik-santulan-score-1-41u3.onrender.com";
 
   const form = document.getElementById("predict-form");
   const submitBtn = document.getElementById("submit-btn");
